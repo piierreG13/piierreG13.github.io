a8ca29aba7772022664720607eeee9ecf5a7f400
piierreG13 <pierregrouard1@gmail.com>
2017-04-29 14:34:25 +0200

    Update custom.js

--- Projet/js/custom.js
+++ Projet/js/custom.js
@@ -1,10 +1,20 @@
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        var myObj = this.responseText;
-        var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
-        document.getElementById("demo").innerHTML = jsonPretty;
-    }
-};
-xmlhttp.open("GET", "https://api.blockcypher.com/v1/btc/main", true);
-xmlhttp.send();
+$(document).ready(function(){
+	$.ajax({
+		url :"https://bitcoin.mubiz.com/info",
+		dataType :"json",
+		contentType :"application/json; charset=utf-8",
+		type :"GET",
+		timeout:"5000",
+		async : false,
+
+		success : function(data) {
+			$('#bitcoin_block_number').append(data.blocks);
+			$('#difficulty').append(data.difficulty);
+		},
+
+		error : function(xhr, status, err) {
+			$('#bitcoin_block_number').append(err+" N/A");
+			$('#difficulty').append(err+" N/A");
+		}
+	});
+});
